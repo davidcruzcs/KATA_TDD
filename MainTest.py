0aff3b8b093743027ca6d53eae10721d0280ff7a
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-03-17 22:19:45 -0500

    Iteración 1 - Paso 2. Se agregar prueba para probar con un numero (Rojo)

--- MainTest.py
+++ MainTest.py
@@ -5,3 +5,6 @@ from Main import Main
 class MainTest(TestCase):
     def test_contarElementos(self):
         self.assertEqual(Main().contarElementos(""),0, "Cadena Vacia")
+
+    def test_contarElementos_UnElemento(self):
+        self.assertEqual(Main().contarElementos("1"), 1, "Un Numero")
