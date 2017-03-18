182627a68683e3c57d55f49565d058aa083d217f
Diego Alfonso Prieto Torres <da.prieto1@uniandes.edu.co>
2017-03-17 22:57:26 -0500

    Iteración 2- Paso 3. Se corrige el método para encontrar el mínimo en un arreglo de dos números (Verde)
    Iteración 2- Paso 3. Se corrige el método para encontrar el mínimo en
    un arreglo de dos números (Verde)

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ class Main:
             return ["0", "0"]
         elif len(cadena) == 1 :
             return [str(len(cadena)), cadena[0]]
+        elif len(cadena) == 2 :
+            return [str(len(cadena)), min(cadena[0], cadena[1])]
         else:
             return [str(len(cadena))]
 
