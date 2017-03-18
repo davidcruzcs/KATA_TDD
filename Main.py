33c2ef1c4a8a044a7995cfbddf0ec9268fdb41f5
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-03-17 22:39:40 -0500

    Iteración 1 - Paso 4. Se modifica código parta que pase prueba con N numeros (Verde)

--- Main.py
+++ Main.py
@@ -2,7 +2,6 @@ class Main:
     def contarElementos(self,cadena):
         if cadena == [""]:
             return ["0"]
-        elif len(cadena) == 1:
-            return ["1"]
-        elif len(cadena) == 2:
-            return ["2"]
+        else:
+            return [str(len(cadena))]
+
