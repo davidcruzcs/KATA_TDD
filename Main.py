efc8dde291a7576a6f383cde833bb13478890b2d
Diego Alfonso Prieto Torres <da.prieto1@uniandes.edu.co>
2017-03-17 23:13:04 -0500

    Iteración 3 - Paso 4. Se corrige el método para encontrar el máximo en un arreglo de n elementos (Verde)
    Iteración 3 - Paso 4. Se corrige el método para encontrar el máximo en
    un arreglo de n elementos (Verde)

--- Main.py
+++ Main.py
@@ -2,10 +2,6 @@ class Main:
     def contarElementos(self,cadena):
         if cadena == [""]:
             return ["0", "0", "0"]
-        elif len(cadena) == 1:
-            return [str(len(cadena)), str(min([int(n) for n in cadena])), cadena[0]]
-        elif len(cadena) == 2:
-            return [str(len(cadena)), str(min([int(n) for n in cadena])), max(cadena[0], cadena[1])]
         else:
-            return [str(len(cadena)), str(min([int(n) for n in cadena]))]
+            return [str(len(cadena)), str(min([int(n) for n in cadena])), str(max([int(n) for n in cadena]))]
 
