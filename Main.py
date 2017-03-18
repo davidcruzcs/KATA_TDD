32a0a1a395c2c0bcd4452e87706dfe3e71089f7a
Diego Alfonso Prieto Torres <da.prieto1@uniandes.edu.co>
2017-03-17 22:53:40 -0500

    Iteración 2- Paso 2. Se corrige el método para devolver el mínimo en un arreglo de 1 número (Verde)

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ class Main:
     def contarElementos(self,cadena):
         if cadena == [""]:
             return ["0", "0"]
+        elif len(cadena) == 1 :
+            return [str(len(cadena)), cadena[0]]
         else:
             return [str(len(cadena))]
 
