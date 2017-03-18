89a8d3a4181c5b5cfd7ff31bdf5caa50f3803bf4
Diego Alfonso Prieto Torres <da.prieto1@uniandes.edu.co>
2017-03-17 23:11:20 -0500

    Iteración 3 - Paso 3. Se corrige el método para encontrar el máximo en un arreglo de dos elementos (Verde)
    Iteración 3 - Paso 3. Se corrige el método para encontrar el máximo en
    un arreglo de dos elementos (Verde)

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ class Main:
             return ["0", "0", "0"]
         elif len(cadena) == 1:
             return [str(len(cadena)), str(min([int(n) for n in cadena])), cadena[0]]
+        elif len(cadena) == 2:
+            return [str(len(cadena)), str(min([int(n) for n in cadena])), max(cadena[0], cadena[1])]
         else:
             return [str(len(cadena)), str(min([int(n) for n in cadena]))]
 
