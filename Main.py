51e13ec3dbc21d35865c933e28f5e536520360e8
Diego Alfonso Prieto Torres <da.prieto1@uniandes.edu.co>
2017-03-17 23:03:46 -0500

    Iteración 2- Paso 4. Se corrige la prueba para encontrar el mínimo en un arreglo de N números (Verde)
    Iteración 2- Paso 4. Se corrige la prueba para encontrar el mínimo en
    un arreglo de N números (Verde)

--- Main.py
+++ Main.py
@@ -2,10 +2,6 @@ class Main:
     def contarElementos(self,cadena):
         if cadena == [""]:
             return ["0", "0"]
-        elif len(cadena) == 1 :
-            return [str(len(cadena)), cadena[0]]
-        elif len(cadena) == 2 :
-            return [str(len(cadena)), min(cadena[0], cadena[1])]
         else:
-            return [str(len(cadena))]
+            return [str(len(cadena)), str(min([int(n) for n in cadena]))]
 
