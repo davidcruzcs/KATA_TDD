960278780eed1e7b754af78a43b525ac1d48bfe5
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-03-19 13:39:41 -0500

    Iteración 4 - Paso 1. Se crea prueba con 0 numeros (Rojo)

--- MainTest.py
+++ MainTest.py
@@ -4,7 +4,7 @@ from Main import Main
 
 class MainTest(TestCase):
     def test_contarElementos(self):
-        self.assertEqual(Main().contarElementos([""]),["0", "0", "0"], "Cadena Vacia")
+        self.assertEqual(Main().contarElementos([""]),["0", "0", "0", "0"], "Cadena Vacia")
 
     def test_contarElementos_UnElemento(self):
         self.assertEqual(Main().contarElementos(["1"]), ["1", "1", "1"], "Un Numero")
