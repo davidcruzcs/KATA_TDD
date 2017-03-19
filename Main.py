f09bb37555705e9a10166b7f23ec96d13ef723d9
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-03-19 13:41:28 -0500

    Iteración 4 - Paso 1. Se corrige código para que pase prueba con 0 numeros (Verde)

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 class Main:
     def contarElementos(self,cadena):
         if cadena == [""]:
-            return ["0", "0", "0"]
+            return ["0", "0", "0", "0"]
         else:
             return [str(len(cadena)), str(min([int(n) for n in cadena])), str(max([int(n) for n in cadena]))]
 
