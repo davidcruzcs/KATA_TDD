4a278f1b890a34c5e27dacc755404b64ccdf68ca
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-03-19 13:59:24 -0500

    Iteración 4 - Paso 1. Se corrige código para que pase prueba con 2 numeros (Verde)

--- Main.py
+++ Main.py
@@ -3,10 +3,19 @@ class Main:
         if cadena == [""]:
             return ["0", "0", "0", "0"]
         else:
-            return [str(len(cadena)),
-                    str(min([int(n) for n in cadena])),
-                    str(max([int(n) for n in cadena])),
-                    "1.0"
-                    ]
+            if len(cadena) <= 2:
+                return [str(len(cadena)),
+                        str(min([int(n) for n in cadena])),
+                        str(max([int(n) for n in cadena])),
+                        str(sum([int(n) for n in cadena]) / float(len(cadena)))
+                        ]
+            else:
+                return [str(len(cadena)),
+                        str(min([int(n) for n in cadena])),
+                        str(max([int(n) for n in cadena])),
+                        "1.0"
+                        ]
+
+
 
 
