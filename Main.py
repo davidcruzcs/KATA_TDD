b377c9cb18b2af783af7ec7688f349958ccd4b82
Juan David Cruz Serrano <juandavidcruzs@gmail.com>
2017-03-19 14:00:41 -0500

    Iteración 4 - Paso 1. Se corrige código para que pase prueba con N numeros (Verde)

--- Main.py
+++ Main.py
@@ -3,18 +3,12 @@ class Main:
         if cadena == [""]:
             return ["0", "0", "0", "0"]
         else:
-            if len(cadena) <= 2:
-                return [str(len(cadena)),
+            return [str(len(cadena)),
                         str(min([int(n) for n in cadena])),
                         str(max([int(n) for n in cadena])),
                         str(sum([int(n) for n in cadena]) / float(len(cadena)))
                         ]
-            else:
-                return [str(len(cadena)),
-                        str(min([int(n) for n in cadena])),
-                        str(max([int(n) for n in cadena])),
-                        "1.0"
-                        ]
+
 
 
 
